e48f2587a1cd1220a93a4d00a28f6a7e16efa735
WilliamFerns1 <willfv2@gmail.com>
2024-04-26 06:44:18 +0200

    feat: empty front-end

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,7 @@
-"use client";
 
-import { populateDatabaseWithArticles } from "@/lib/helpers";
-import {useEffect, useState} from "react";
 export default function Home() {
-  const [articles, setArticles] = useState([]);
-
-  const populateDB = async () => {
-    if (articles.length === 0) {
-      const result : any = await populateDatabaseWithArticles();
-      console.log(result)
-      setArticles(result);
-    }
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button 
-        className="bg-blue-500 px-[15px] py-[8px] rounded-[5px] font-bold text-black"
-        onClick={populateDB}
-      >
-        Populate DB
-      </button>
-
-      {
-        articles && (
-          <div className="text-white">{JSON.stringify(articles)}</div>)
-      }
     </main>
   );
 }
